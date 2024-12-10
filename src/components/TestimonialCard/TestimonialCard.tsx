3bed4bbc83a479416df9c44a272aa30c564d0d86
Felipe Souza <fgsouza93@gmail.com>
2024-12-09 22:09:23 -0300

    refactor: use semantic html

--- src/components/TestimonialCard/TestimonialCard.tsx
+++ src/components/TestimonialCard/TestimonialCard.tsx
@@ -15,16 +15,18 @@ function TestimonialCard({
   imageUrl,
 }: TestimonalCardProps) {
   return (
-    <div className="flex flex-col gap-4 border w-[340px] p-5 bg-white rounded-lg shadow-md ">
-      <div className="flex align-middle gap-4">
+    <figure className="flex flex-col gap-4 border w-[340px] p-5 sm:bg-white bg-black rounded-lg shadow-md ">
+      <figcaption className="flex align-middle gap-4">
         <img className="h-16 w-16 rounded-full" src={imageUrl} alt="avatar" />
         <div className="flex flex-col justify-center align-middle truncate">
           <p className="text-primary font-bold text-lg truncate">{title}</p>
           <p className="text-tertiary truncate">{subtitle}</p>
         </div>
-      </div>
-      <p className="text-secondary">{text}</p>
-    </div>
+      </figcaption>
+      <blockquote>
+        <p className="text-secondary">{text}</p>
+      </blockquote>
+    </figure>
   );
 }
 
